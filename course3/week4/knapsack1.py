976920a7482fa37d18a3ba43a9efaba0547acbd4
tharoorpoojasuresh <tharoorpoojasuresh@gmail.com>
2017-09-11 12:21:17 +0530

    Update knapsack1.py

--- course3/week4/knapsack1.py
+++ course3/week4/knapsack1.py
@@ -11,24 +11,24 @@ def readfile():
  M = int(l1[0])
  n = int(l1[1])
  i = 1
-for l in fp:
- obj = l.strip().split(" ")
- values[i] = int(obj[0])
- weights[i] = int(obj[1])
- i = i + 1
-fp.close()
+ for l in fp:
+  obj = l.strip().split(" ")
+  values[i] = int(obj[0])
+  weights[i] = int(obj[1])
+  i = i + 1
+ fp.close()
 
 def knapsack1():
  for i in range(1,n+1):
   profit.append([0])
  for j in range(1,M+1):
   profit[0].append(0)
-for i in range(1,n+1):
- for w in range(M+1):
-  if weights[i] > w:
-   profit[i].append(profit[i-1][w])
-  else:
-   profit[i].append(max(profit[i-1][w], values[i] + profit[i-1][w-weights[i]))
+ for i in range(1,n+1):
+  for w in range(M+1):
+   if weights[i] > w:
+    profit[i].append(profit[i-1][w])
+   else:
+    profit[i].append(max(profit[i-1][w], values[i] + profit[i-1][w-weights[i]))
  print ("knapsack solution = ", profit[n][M])
 
 
@@ -38,5 +38,7 @@ def main():
 
 if __name__ == "__main__":
  main()
+
+ 
 
  
