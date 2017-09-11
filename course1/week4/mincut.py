996151f8575ff9a8a5d30c717f2552f112fc2842
tharoorpoojasuresh <tharoorpoojasuresh@gmail.com>
2017-09-11 11:04:33 +0530

    Update mincut.py

--- course1/week4/mincut.py
+++ course1/week4/mincut.py
@@ -13,25 +13,26 @@ def readfile():
   e=l.split("\t")
   v1=int(e[0])
   gr[v1]={}
- for i in range(1,len(e)):
-  v2=int(e[i])
-  gr[v1][v2]=1
+  for i in range(1,len(e)):
+   v2=int(e[i])
+   gr[v1][v2]=1
+   m=m+1
  n=n+1
- m=m+1
-fp.close()
+ 
+ fp.close()
 
 def mincut(g):
  random.seed()
  while len(g) > 2:
   v1= random.choice(list(g.keys()))
   v2= random.choice(list(g[v1].keys()))
-   for v in g[v2]:
+  for v in g[v2]:
     g[v].pop(v2)
     if v != v1:
      g[v][v1] = g[v].setdefault(v1, 0) + g[v2][v]
      g[v1][v] = g[v1].setdefault(v, 0) + g[v2][v]
              
-     g.pop(v2)
+   g.pop(v2)
         
         
         
@@ -40,7 +41,7 @@ def mincut(g):
   
   v2 = list(g[v1].keys())[0]
  
- return g[v1][v2]
+  return g[v1][v2]
  
 
 def main():
@@ -50,14 +51,21 @@ def main():
  for j in range(input):
   graph=copy.deepcopy(gr)
   mcut = mincut(graph)
- if mincuts == 0:
-  mincuts = mcut
- elif mincuts > mcut:
-  mincuts = mcut
- print("KargerMinCut = ", mincuts)
+  if mincuts == 0:
+   mincuts = mcut
+  elif mincuts > mcut:
+   mincuts = mcut
+  print("KargerMinCut = ", mincuts)
 
 if __name__=="__main__":
  main()
+
+
+
+ 
+
+
+
 
 
 
